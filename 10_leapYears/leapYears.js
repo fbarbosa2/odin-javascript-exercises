1d87ff7d3949d2d8eed0bab034d0499911d381cf
Francisco <149792958+fbarbosa2@users.noreply.github.com>
2025-09-26 15:27:31 +0100

    completed exercises from 06 to 11

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    isLeap = false;
+    if(year % 4){
+        isLeap = true;
+    }
 
+    if(year % 100 && year % 400){
+        isLeap = true;
+    }
+    return isLeap;
 };
 
 // Do not edit below this line
